281a7a6b3ca062fd54cd14ea682af3b1eedd2a66
Max Burrows <maxajburrows@gmail.com>
2023-02-06 20:01:31 +0100

    Start adding square selection functionality

--- js_file.js
+++ js_file.js
@@ -1,10 +1,37 @@
 
+
+
 let gameBoard = (function () {
+    let arrayGameBoard = new Array(9).fill('empty');
     let container = document.querySelector('.container');  
     for (let i = 1; i <= 9; i++) {
         let square = document.createElement('div');
         square.setAttribute('class', 'square');
+        square.setAttribute('id', `${i}`);
+        square.addEventListener('click', player.playerClick)
         container.appendChild(square);
     }
+    return {arrayGameBoard};
 })();
 
+
+let player = function () {
+    let playerOneTurn = true;
+    let playerTwoTurn = false;
+    let playerClick = function () {
+        if (playerOneTurn) {
+            this.textContent = 'X';
+            playerOneTurn = false;
+            playerTwoTurn = true;
+        }
+        else {
+            this.textContent = 'O';
+            playerOneTurn = true;
+            playerTwoTurn = false;
+        }
+        this.removeEventListener('click', player.playerClick)
+
+    }
+
+    return {playerClick}
+}
