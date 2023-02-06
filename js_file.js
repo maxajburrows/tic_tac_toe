f2127149a5a1077e9975470dcdb5201e8899cd6e
Max Burrows <maxajburrows@gmail.com>
2023-02-06 22:18:52 +0100

    Update display gameboard and logic gameboard with user click

--- js_file.js
+++ js_file.js
@@ -1,21 +1,7 @@
 
 
 
-let gameBoard = (function () {
-    let arrayGameBoard = new Array(9).fill('empty');
-    let container = document.querySelector('.container');  
-    for (let i = 1; i <= 9; i++) {
-        let square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        square.setAttribute('id', `${i}`);
-        square.addEventListener('click', player.playerClick)
-        container.appendChild(square);
-    }
-    return {arrayGameBoard};
-})();
-
-
-let player = function () {
+let player = (function () {
     let playerOneTurn = true;
     let playerTwoTurn = false;
     let playerClick = function () {
@@ -23,15 +9,40 @@ let player = function () {
             this.textContent = 'X';
             playerOneTurn = false;
             playerTwoTurn = true;
+            editBoard('playerOne', this.id);
         }
         else {
             this.textContent = 'O';
             playerOneTurn = true;
             playerTwoTurn = false;
+            editBoard('playerTwo', this.id);
         }
         this.removeEventListener('click', player.playerClick)
-
     }
+    console.log(playerClick)
 
     return {playerClick}
-}
+})();
+
+console.log(player);
+
+
+let gameBoard = (function () {
+    let arrayGameBoard = new Array(9).fill('');
+    let container = document.querySelector('.container');  
+    for (let i = 1; i <= 9; i++) {
+        let square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.setAttribute('id', `${i}`);
+        square.addEventListener('click', player.playerClick);
+        container.appendChild(square);
+    }
+    let editBoard = function (player, squareNum) {
+        if (player === 'playerOne') {
+            arrayGameBoard[squareNum] = 'cross';
+        } else if (player === 'playerTwo') {
+            arrayGameBoard[squareNum] = 'nought';
+        }
+    }
+    return {arrayGameBoard, editBoard};
+})();
